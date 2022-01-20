80d69a3d1a69126077e92b948723e72ddcf64352
denio-rus <denio@yandex.ru>
2022-01-20 16:28:01 +0300

    Refact

--- lib/be_gateway.rb
+++ lib/be_gateway.rb
@@ -1,17 +1,17 @@
 require 'faraday'
 require 'faraday_middleware'
 require 'ostruct'
-require 'be_gateway/version'
+require 'be_gateway_v2/version'
 require 'backports/2.0.0/stdlib/ostruct'
 
 module BeGatewayV2
-  autoload :Connection, "be_gateway/connection"
-  autoload :Client, "be_gateway/client"
-  autoload :AsyncClient, "be_gateway/async_client"
-  autoload :Checkout, "be_gateway/checkout"
-  autoload :Base, "be_gateway/response/base"
-  autoload :Response, "be_gateway/response/response"
-  autoload :ErrorResponse, "be_gateway/response/error_response"
-  autoload :Transaction, "be_gateway/response/transaction"
-  autoload :AsyncResponse, "be_gateway/response/async_response"
+  autoload :Connection, "be_gateway_v2/connection"
+  autoload :Client, "be_gateway_v2/client"
+  autoload :AsyncClient, "be_gateway_v2/async_client"
+  autoload :Checkout, "be_gateway_v2/checkout"
+  autoload :Base, "be_gateway_v2/response/base"
+  autoload :Response, "be_gateway_v2/response/response"
+  autoload :ErrorResponse, "be_gateway_v2/response/error_response"
+  autoload :Transaction, "be_gateway_v2/response/transaction"
+  autoload :AsyncResponse, "be_gateway_v2/response/async_response"
 end
